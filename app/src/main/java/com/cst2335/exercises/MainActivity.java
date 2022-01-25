a46191bd2843f8062b90efa4dc54d1bfe90ec72a
Samira Ouaaz <souaaz@yahoo.com>
2022-01-25 00:42:46 -0500

    linear layout

--- app/src/main/java/com/cst2335/exercises/MainActivity.java
+++ app/src/main/java/com/cst2335/exercises/MainActivity.java
@@ -1,45 +1,51 @@
 package com.cst2335.exercises;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear); //shows the layout
 
-        //now your xml is loaded
-        TextView topView = findViewById(R.id.helloTextView); //must match XML id
-        String oldText = topView.getText().toString();
-        topView.setText("Java put this here");
+        TextView myText = findViewById(R.id.text);//brings the textView from XML to Java
+        myText.setText(R.string.hello_world);//nullPointer Exception
 
-        EditText bottom = findViewById(R.id.bottomtext);
+        EditText myEdit = findViewById(R.id.edit);
+        RadioButton myCb = findViewById(R.id.myCb);
 
-        Button btn = findViewById(R.id.button);
-        btn.setText("The view was previously "+ oldText); //on startup
+        Button myBtn = findViewById(R.id.btn);
+        myBtn.setOnClickListener((vw) -> {
+            myText.setText("You clicked the button!");
+            myCb.setChecked(false);
 
-        /*
-        btn.setOnClickListener(  (  click ) ->
-        {
-            //when  you click
-            //text is empty
-            topView.setText("Edit text has " +  bottom.getText().toString());
 
-        }   ); //OnCLickListener goes in here
-        */
-        btn.setOnClickListener(new View.OnClickListener() { //anonymous class
-            @Override
-            public void onClick(View v) {
-                topView.setText("Edit text has " + bottom.getText());
-            }
+            // Toast.makeText(MainActivity.this, " ",Toast.LENGTH_LONG).show();
+            Snackbar.make(myText, "hello snack", Snackbar.LENGTH_LONG).show();
         });
+        ImageButton myImgBtn = findViewById(R.id.imgView);
 
+        myImgBtn.setOnClickListener((view) -> myEdit.setText("You clicked the image"));
+
+        myCb.setOnCheckedChangeListener((btnView, onOrOff)-> {
+            myBtn.setText(onOrOff?"box is on" : "box is off");
+
+        });
     }
 }
