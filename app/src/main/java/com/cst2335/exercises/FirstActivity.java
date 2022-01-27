3df15c71bc816bf278d2f89e4196b91834306b56
Samira Ouaaz <souaaz@yahoo.com>
2022-01-26 22:23:06 -0500

    2 activities

---
+++ app/src/main/java/com/cst2335/exercises/FirstActivity.java
@@ -0,0 +1,49 @@
+package com.cst2335.exercises;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Your program starts here
+        super.onCreate(savedInstanceState);
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
+        setContentView(R.layout.activity_main);
+        Button secondButton = findViewById(R.id.buttonToSecond);
+        EditText inputText = findViewById(R.id.inputText);
+
+        //This creates a transition to load SecontActivity.java:
+        Intent nextPage = new Intent(this, SecondActivity.class);
+        //when you click the button, start the next activity:
+        secondButton.setOnClickListener( click ->
+        {
+            nextPage.putExtra("name", "me");      //name = Eric
+            nextPage.putExtra("age", 20);           // age = 20
+            nextPage.putExtra("typed", inputText.getText().toString() ); //typed = what user typed
+            startActivity(nextPage);    //go to SecondActivity.java
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
+
---
+++ app/src/main/java/com/cst2335/exercises/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.cst2335.exercises;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent(); //This gets you the object nextPage from FirstActivity.java, line 36
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age", 0);
+        //    String typed = intent. ??? // what goes here?
+
+        Button previousButton = findViewById(R.id.previousPageButton);
+
+        previousButton.setOnClickListener(click -> {
+            Intent dataSent = new Intent();
+
+            finish();
+        } );
+
+    }
+}
