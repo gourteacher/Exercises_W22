d54dc3723fb7291d8fad997da8c9e627ff64e7bc
Samira Ouaaz <souaaz@yahoo.com>
2022-02-09 19:48:02 -0500

    Week 5 - Floating Action Button + ArrayAdapter

--- app/src/main/java/com/cst2335/exercises/FirstActivity.java
+++ app/src/main/java/com/cst2335/exercises/FirstActivity.java
@@ -1,111 +1,63 @@
 package com.cst2335.exercises;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import java.util.ArrayList;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class FirstActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
-    private ArrayList<String> elements = new ArrayList<>(  );
-    MyListAdapter myAdapter;
+    ArrayList<String> listItems = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Your program starts here
         super.onCreate(savedInstanceState);
-
-        // setContentView loads objects onto the screen.
-        // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
 
-        ListView myList = findViewById(R.id.theListView);
-        //Line 37 is the same as lines 35 and 36:
-        //ListAdapter myAdapter = new MyListAdapter();
-        //myList.setAdapter( myAdapter);
-        myList.setAdapter( myAdapter = new MyListAdapter());
+        myListView = findViewById(R.id.myListView);
 
-        Button addButton = findViewById(R.id.addButton);
-        addButton.setOnClickListener( click -> {
-            elements.add( "Another Row");
-            myAdapter.notifyDataSetChanged();
+        adapter = new ArrayAdapter<>( this,
+                android.R.layout.simple_dropdown_item_1line,
+                listItems);
+
+        myListView.setAdapter(adapter);
+
+        View.OnClickListener undo = view -> {
+            listItems.remove(listItems.size() -1);
+            adapter.notifyDataSetChanged();
+            Snackbar.make(view, "Item removed", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        };
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            addListItem();
+            Snackbar.make(view, "Item added to list",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+                    //.setAction("Undo", undo).show();
         });
-
-        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                elements.remove(i);
-                myAdapter.notifyDataSetChanged();
-            }
-        });
-        
-        myList.setOnItemLongClickListener( (p, b, pos, id) -> {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("Make a choice")
-
-                    //What is the message:
-                    .setMessage("Do you want to add a row")
-
-                    //what the Yes button does:
-                    .setPositiveButton("Yes", (click, arg) -> {
-                        elements.add("HELLO");
-                        myAdapter.notifyDataSetChanged();
-                    })
-                    //What the No button does:
-                    .setNegativeButton("No", (click, arg) -> { })
-
-                    //An optional third button:
-                    .setNeutralButton("Maybe", (click, arg) -> {  })
-
-                    //You can add extra layout elements:
-                    .setView(getLayoutInflater().inflate(R.layout.row_layout, null) )
-
-                    //Show the dialog
-                    .create().show();
-            return true;
-        });
-
-
 
     }
 
-    private class MyListAdapter extends BaseAdapter {
-
-        public int getCount() { return elements.size();}
-
-        public Object getItem(int position) { return "This is row " + position; }
-
-        public long getItemId(int position) { return (long) position; }
-
-        public View getView(int position, View old, ViewGroup parent)
-        {
-            LayoutInflater inflater = getLayoutInflater();
-
-            //make a new row:
-            View newView = inflater.inflate(R.layout.row_layout, parent, false);
-
-            //set what the text should be for this row:
-            TextView tView = newView.findViewById(R.id.textGoesHere);
-            tView.setText( getItem(position).toString() );
-
-            //Button b =  newView.findViewById(R.id.textGoesHere);
-            //b.setText( getItem(position).toString() );
-
-            //return it to be put in the table
-            return newView;
-        }
+    private void addListItem() {
+        SimpleDateFormat dateformat =
+                new SimpleDateFormat("HH:mm:ss MM/dd/yyyy",
+                        Locale.US);
+        listItems.add(dateformat.format(new Date()));
+        adapter.notifyDataSetChanged();
     }
 }
 
