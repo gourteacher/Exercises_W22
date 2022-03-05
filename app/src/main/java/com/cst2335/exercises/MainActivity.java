0c9b5e1a56d2621bf07479a4a92525c4faae8932
gourteacher <ouaazs@algonquincollege.com>
2022-03-04 19:13:31 -0500

    Adding http

--- app/src/main/java/com/cst2335/exercises/MainActivity.java
+++ app/src/main/java/com/cst2335/exercises/MainActivity.java
@@ -1,10 +1,19 @@
 package com.cst2335.exercises;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +23,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
+        req.execute("https://google.com");  //Type 1
     }
-                                                //Type1     Type2   Type3
+
+
+    //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
-               //Type3                Type1
+        //Type3                Type1
         public String doInBackground(String ... args)
         {
+            try {
+
+                //create a URL object of what server to contact:
+                URL url = new URL(args[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+                //JSON reading:   Look at slide 26
+                //Build the entire string response:
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                String result = sb.toString(); //result is the whole string
+
+
+                // convert string to JSON: Look at slide 27:
+                JSONObject uvReport = new JSONObject(result);
+
+                //get the double associated with "value"
+                double uvRating = uvReport.getDouble("value");
+
+                Log.i("MainActivity", "The uv is now: " + uvRating) ;
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
             return "Done";
         }
 
@@ -30,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         {
 
         }
-                                //Type3
+        //Type3
         public void onPostExecute(String fromDoInBackground)
         {
             Log.i("HTTP", fromDoInBackground);
