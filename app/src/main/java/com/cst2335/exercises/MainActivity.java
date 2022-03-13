b47065aa84914aeb978b1b8b4084478a234559c5
gourteacher <ouaazs@algonquincollege.com>
2022-03-13 00:10:28 -0500

    Basic AsyncTask

--- app/src/main/java/com/cst2335/exercises/MainActivity.java
+++ app/src/main/java/com/cst2335/exercises/MainActivity.java
@@ -5,14 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.json.JSONObject;
-
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,53 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("https://api.publicapis.org/entries");  //Type 1
+        new MyTask().execute("This is MyTask parameter");
     }
 
 
     //Type1     Type2   Type3
-    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
+    private class MyTask extends AsyncTask< String, Integer, String>
     {
-        static private final String TAG = "MyHTTPRequest";
+        static private final String TAG = "MyTask";
 
         //Type3                Type1
+        @Override
         public String doInBackground(String ... args)
         {
             try {
 
                 //create a URL object of what server to contact:
-                URL url = new URL(args[0]);
-
-                //open the connection
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                //wait for data:
-                InputStream response = urlConnection.getInputStream();
-
-                //JSON reading:
-                //Build the entire string response:
-                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-                StringBuilder sb = new StringBuilder();
-
-                String line = null;
-                while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line + "\n");
-                }
-                String result = sb.toString(); //result is the whole string
-
-
-                // convert string to JSON: Look at slide 27:
-                JSONObject uvReport = new JSONObject(result);
-
-                //get the double associated with "value"
-                int numEntries = uvReport.getInt("count");
+                String myString = args[0];
 
                 publishProgress(25);
                 Thread.sleep(1000);
                 publishProgress(50);
-                Log.i(TAG, "Num of entries: " + numEntries) ;
+                Thread.sleep(1000);
+                publishProgress(75);
 
             }
             catch (Exception e)
