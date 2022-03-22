eea060a2e0bcd364591f99200fd51fa3b564e1dc
gourteacher <ouaazs@algonquincollege.com>
2022-03-22 11:49:17 -0400

    Navigation Drawer Example

--- app/src/main/java/com/cst2335/exercises/MenuExample.java
+++ app/src/main/java/com/cst2335/exercises/MenuExample.java
@@ -1,15 +1,19 @@
 package com.cst2335.exercises;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MenuExample extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MenuExample extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,49 +23,51 @@ public class MenuExample extends AppCompatActivity {
         //This gets the toolbar from the layout:
         Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(tBar);
+
+        //For NavigationDrawer:
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
-
+    // Needed for the OnNavigationItemSelected interface:
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_menu, menu);
-        return true;
-    }
+    public boolean onNavigationItemSelected( MenuItem item) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
         String message = null;
-        //Look at your menu XML file. Put a case for every id in that file:
+
         switch(item.getItemId())
         {
-            //what to do when the menu item is selected:
-            case R.id.clear_item:
-                message = "You clicked on clear";
+            case R.id.share_item:
+                message = "You clicked on share";
                 break;
-            case R.id.search_item:
-                message = "You clicked on search";
-                break;
-            case R.id.help_item:
-                message = "You clicked on help";
-                break;
-            case R.id.mail_item:
-                message = "You clicked on mail";
+            case R.id.app_bar_search:
+                message = "You clicked on the search";
                 break;
             case R.id.video_item:
                 message = "You clicked on video";
                 break;
+            case R.id.mail_item:
+                message = "You clicked on mail";
+                break;
             case R.id.call_item:
                 message = "You clicked on call";
-                Intent i = new Intent (getApplicationContext(), SomeActivity.class);
-                startActivity(i);
                 break;
         }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
         if ( message != null ) {
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "NavigationDrawer: " + message, Toast.LENGTH_LONG).show();
         }
-        return true;
+        return false;
     }
 
 }
